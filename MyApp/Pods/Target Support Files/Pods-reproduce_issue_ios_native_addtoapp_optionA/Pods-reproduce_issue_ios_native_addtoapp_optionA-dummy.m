34efd439dd776413ef60f53eb26ad1c37dd9d2bb
Nguyen Huy <huy@nevercode.io>
2023-03-24 11:02:43 +0700

    first commit

---
+++ MyApp/Pods/Target Support Files/Pods-reproduce_issue_ios_native_addtoapp_optionA/Pods-reproduce_issue_ios_native_addtoapp_optionA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reproduce_issue_ios_native_addtoapp_optionA : NSObject
+@end
+@implementation PodsDummy_Pods_reproduce_issue_ios_native_addtoapp_optionA
+@end
---
+++ MyApp/reproduce_issue_ios_native_addtoapp_optionA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reproduce_issue_ios_native_addtoapp_optionA
+//
+//  Created by HuyNQ on 23/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
