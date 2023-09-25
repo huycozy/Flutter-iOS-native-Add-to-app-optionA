5d7917da4265996676e920380e493b5d018ebae0
Nguyen Huy <huy@nevercode.io>
2023-09-25 23:26:25 +0700

    update - done demo

--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -1,112 +1,166 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+/// The entrypoint for the flutter module.
+void main() {
+  // This call ensures the Flutter binding has been set up before creating the
+  // MethodChannel-based model.
+  WidgetsFlutterBinding.ensureInitialized();
 
+  final model = CounterModel();
+
+  runApp(
+    ChangeNotifierProvider.value(
+      value: model,
+      child: const MyApp(),
+    ),
+  );
+}
+
+/// A simple model that uses a [MethodChannel] as the source of truth for the
+/// state of a counter.
+///
+/// Rather than storing app state data within the Flutter module itself (where
+/// the native portions of the app can't access it), this module passes messages
+/// back to the containing app whenever it needs to increment or retrieve the
+/// value of the counter.
+class CounterModel extends ChangeNotifier {
+  CounterModel() {
+    _channel.setMethodCallHandler(_handleMessage);
+    _channel.invokeMethod<void>('requestCounter');
+  }
+
+  final _channel = const MethodChannel('dev.flutter.example/counter');
+
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _channel.invokeMethod<void>('incrementCounter');
+  }
+
+  Future<dynamic> _handleMessage(MethodCall call) async {
+    if (call.method == 'reportCounter') {
+      _count = call.arguments as int;
+      notifyListeners();
+    }
+  }
+}
+
+/// The "app" displayed by this module.
+///
+/// It offers two routes, one suitable for displaying as a full screen and
+/// another designed to be part of a larger UI.class MyApp extends StatelessWidget {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Module Title',
+      theme: ThemeData.light(useMaterial3: true),
+      routes: {
+        '/': (context) => const FullScreenView(),
+        '/mini': (context) => const Contents(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+/// Wraps [Contents] in a Material [Scaffold] so it looks correct when displayed
+/// full-screen.
+class FullScreenView extends StatelessWidget {
+  const FullScreenView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Full-screen Flutter'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: const Contents(showExit: true),
+    );
+  }
+}
+
+/// The actual content displayed by the module.
+///
+/// This widget displays info about the state of a counter and how much room (in
+/// logical pixels) it's been given. It also offers buttons to increment the
+/// counter and (optionally) close the Flutter view.
+class Contents extends StatelessWidget {
+  final bool showExit;
+
+  const Contents({super.key, this.showExit = false});
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaInfo = MediaQuery.of(context);
+
+    return SizedBox.expand(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Theme.of(context).scaffoldBackgroundColor,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const Positioned.fill(
+            child: Opacity(
+              opacity: .25,
+              child: FittedBox(
+                fit: BoxFit.cover,
+                child: FlutterLogo(),
+              ),
             ),
-          ],
-        ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Window is ${mediaInfo.size.width.toStringAsFixed(1)} x '
+                  '${mediaInfo.size.height.toStringAsFixed(1)}',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const SizedBox(height: 16),
+                Consumer<CounterModel>(
+                  builder: (context, model, child) {
+                    return Text(
+                      'Taps: ${model.count}',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    );
+                  },
+                ),
+                const SizedBox(height: 16),
+                Consumer<CounterModel>(
+                  builder: (context, model, child) {
+                    return ElevatedButton(
+                      onPressed: () => model.increment(),
+                      child: const Text('Tap me!'),
+                    );
+                  },
+                ),
+                if (showExit) ...[
+                  const SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () => SystemNavigator.pop(animated: true),
+                    child: const Text('Exit this screen'),
+                  ),
+                ],
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
